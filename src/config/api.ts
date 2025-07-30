8ddc44b507de2894a3220ebc04f12d6711664d54
Luisj950 <lajaramillo@sudamericano.edu.ec>
2025-07-30 00:55:32 -0500

    DESPLIEGUE 4

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,5 +2,5 @@
 
 export const API_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://backend-final-despliegue.vercel.app' // 🔁 Cambia esto por tu URL real de backend desplegado
-    : 'http://127.0.0.1:3000'; // Localhost para desarrollo
+    ? 'https://backend-final-despliegue.onrender.com' // ✅ URL real del backend en Render
+    : 'http://127.0.0.1:3000'; // ✅ localhost para desarrollo
